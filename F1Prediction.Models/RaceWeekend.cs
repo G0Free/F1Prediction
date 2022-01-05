fc79d5fa33d29717e5948d7456147e79a18abee0
G0Free <gyorfi.bence@gmail.com>
2022-01-05 12:17:54 +0100

    Raceweekend added

---
+++ F1Prediction.Models/RaceWeekend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1Prediction.Models
+{
+    class RaceWeekend
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Practice1 { get; set; }
+        public DateTime Practice2 { get; set; }
+        public DateTime Practice3 { get; set; }
+        public DateTime Qualifying { get; set; }
+        public DateTime Race { get; set; }
+
+    }
+}
---
+++ F1Prediction.Models/RaceWeekendEvents/Practice3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1Prediction.Models
+{
+    class Practice3
+    {
+        public DateTime Date { get; set; }
+    }
+}
